8fd212825b70e7b46e99ddcfdab94d35819cad1f
yiyucsu <yiyucsu@gmail.com>
2015-01-27 12:06:59 -0600

    fix broken country data files

--- setupProject.sh
+++ setupProject.sh
@@ -2,21 +2,22 @@
 
 ##########################################################
 # This script will download the split gap minder zip file,
-# unzip it, 
+# unzip it,makes some directories, and backs up the data
+#into an archive folder 
 ##########################################################
 
 
 echo Creating project directory structure
-sleep 2
-mkdir -v data code output data/archive
+sleep 2  #pause for a while
+mkdir -v data code output data/archive #make a directory under data
 
 cd data
 
 # Fetch the zip file
-ZIP_FILE_URL="http://npk.io/1jHRK+"
-ZIP_FILE_NAME="rawSplitCountries.zip"
+ZIP_FILE_URL="http://npk.io/1jHRK+"   # download data from internet
+ZIP_FILE_NAME="rawSplitCountries.zip" # save the file name as 
 
-echo Downloading zipped data file from $ZIP_FILE_URL
+echo Downloading zipped data file from $ZIP_FILE_URL 
 sleep 2
 curl -L $ZIP_FILE_URL -o $ZIP_FILE_NAME
 
@@ -26,8 +27,8 @@ unzip $ZIP_FILE_NAME
 
 echo Copying the zip file contents to the archive directory
 sleep 2
-cp -vr clean archive/
+cp -vr clean archive/ # copy the whole folder into archive
 
 # Move back up a dir
-cd ../
+cd ../  # move back the root directory
 
